f1c0dce3d3045930b3d2f63ec25b98d7b1f55f21
SAAD TAHIR <saad.tahir.dev@gmail.com>
2024-05-23 17:35:25 +0500

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Saad Tahir on 23/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
